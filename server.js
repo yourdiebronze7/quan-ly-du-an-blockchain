853d7d2ea9596b45ed758880b09adf2f9884a7c0
yourdiebronze7 <sidnyjernov@hotmail.com>
2025-10-01 15:19:17 +0000

    Fix the MongoDB connection URI to be more robust by including useCreateIndex option.

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ app.use(express.json());
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/quanlyduan', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  useCreateIndex: true,
 }).then(() => console.log('MongoDB connected...'))
   .catch(err => console.log(err));
 
